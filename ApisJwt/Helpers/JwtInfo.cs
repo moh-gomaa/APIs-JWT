9e615e39f19834167bee1c229ddd3b4738c1abe9
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2023-07-14 05:36:30 +0300

    -Add the needed package to start the JWT authentication. Read the readme file to know the installed packages. -Add the JWT info to appsettings.json file. -Configure the project to map the JWT section in appsettings.json into the JwtInfo class.

---
+++ ApisJwt/Helpers/JwtInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace ApisJwt.Helpers
+{
+    public class JwtInfo
+    {
+        public string SecretKey { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public string DurationInDays { get; set; } = null!;
+    }
+}
--- ApisJwt/Program.cs
+++ ApisJwt/Program.cs
@@ -1,6 +1,10 @@
+using ApisJwt.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<JwtInfo>(builder.Configuration.GetSection("JWT"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
